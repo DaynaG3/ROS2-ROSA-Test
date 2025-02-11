f17339c34400bb3cc5ada198fa68f212efa79454
Dayna <simru0411@gmail.com>
2025-02-11 11:24:34 +0800

    Delete build directory

--- build/turtle_agent/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ras/isaac_ws/rosa2_mod/src/turtle_agent
-# Build directory: /home/ras/isaac_ws/rosa2_mod/build/turtle_agent
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
